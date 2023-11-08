0ea01eafee095a611facd29aac4948fe54112e3a
Nilliam Ometto <nilliamometto@gmail.com>
2023-11-07 21:16:07 -0300

    feat: sidebar styling

--- src/scenes/global/SideBar.tsx
+++ src/scenes/global/SideBar.tsx
@@ -1,13 +1,47 @@
-import { useState } from 'react';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import { Box, IconButton, useTheme, Typography } from "@mui/material";
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ContactOutlinedIcon from "@mui/icons-material/ContactOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopletOutlined";
+import CalendarOutlinedIcon from "@mui/icons-material/CalendartOutlined";
 
 const SideBar = () => {
-    return <div>SideBar</div>
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [collapsed, setCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Home");
+
+  return (
+    <Sidebar>
+      <Menu>
+        <Box
+          sx={{
+            "& .pro-sidebar-inner": {
+              background: `${colors.primary[400]} !important`,
+            },
+            "& .pro-icon-wrapper": {
+              background: "transparent !important",
+            },
+            "& .pro-inner-item": {
+              background: "5px 35px 5px 20px !important",
+            },
+            "& .pro-inner-item:hover": {
+              color: "#868dfb !important",
+            },
+            "& .pro-menu-item.active": {
+              color: "#6870fa !important",
+            },
+          }}
+        ></Box>
+      </Menu>
+    </Sidebar>
+  );
+};
 
 export default SideBar;
