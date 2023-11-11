eeac86856350d5d88ebef30ee11e4337e8017b14
Nilliam Ometto <nilliamometto@gmail.com>
2023-11-11 18:14:54 -0300

    feat: main scenes routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./App.css";
 import TopBar from "./scenes/global/TopBar";
-import SideBar from "./scenes/global/SideBar";
+import { Route, Routes } from "react-router-dom";
+import Tabs from "./scenes/Tabs";
+import Categories from "./scenes/Categories";
+import Products from "./scenes/Products";
 
-function App() { 
+function App() {
   const [theme, colorMode] = useMode();
 
   return (
@@ -12,10 +15,15 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <SideBar />
-          <main className="content">
-          <TopBar />
+          {/* <SideBar /> */}
 
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Tabs/>} />
+              <Route path="/categories" element={<Categories/>} />
+              <Route path="/products" element={<Products/>} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
