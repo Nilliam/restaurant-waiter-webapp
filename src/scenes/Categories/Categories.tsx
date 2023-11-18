b4565eaef0cc000a5d4421c697abb939fd0cf80f
Nilliam Ometto <nilliamometto@gmail.com>
2023-11-18 18:37:18 -0300

    feat: fetching categories from api

--- src/scenes/Categories/Categories.tsx
+++ src/scenes/Categories/Categories.tsx
@@ -3,15 +3,25 @@ import Card from "@mui/material/Card";
 import { tokens } from "../../theme";
 import { MenuBookOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Categories = () => {
-  var things = ["Beverages", "Snacks", "Sandwiches", "Vegan"];
+  
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [categories, setCategories] = useState<any[]>([]);
+  const url = import.meta.env.VITE_APP_URL;
+
+  useEffect(() => {
+    fetch(`${url}/categories`)
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
+  
   return (
     <Box p={2}>
       <Grid container spacing={2}>
-        {[...Array(20)].map((_, index) => (
+        {categories.map((category, index) => (
           <Grid item xs={6} sm={4} md={4} lg={3} xl={2} key={index}>
             <Card
               sx={{
@@ -34,7 +44,7 @@ const Categories = () => {
                 <MenuBookOutlined sx={{ position: "left", top: 0 }} />
 
                 <CardContent sx={{ textAlign: "center" }}>
-                  {things[Math.floor(Math.random() * things.length)]}
+                  {category.name}
                 </CardContent>
               </Button>
             </Card>
