408fdfb9edb775d3cb76046323c9699ad7c52a70
Nilliam Ometto <nilliamometto@gmail.com>
2023-11-26 18:26:57 -0300

    feat: pushing toasts

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Categories from "./scenes/Categories";
 import Products from "./scenes/Products";
 import { CartProvider } from "./context/CartContext";
 import Cart from "./scenes/Cart/Cart";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -29,6 +31,18 @@ function App() {
                 <Route path="/products/:categoryId" element={<Products />} />
               </Routes>
             </main>
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+            />
           </div>
         </ThemeProvider>
       </ColorModeContext.Provider>
